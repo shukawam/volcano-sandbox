9ebf403cfc2332a8f50d543853ef74dad9b0d86f
shukawam <shukawam+github@gmail.com>
2025-10-23 17:26:00 +0900

    Added sample of use kong gateway ai proxy

---
+++ src/use-kong-gateway-ai-proxy.ts
@@ -0,0 +1,50 @@
+import { agent, llmOpenAI, createVolcanoTelemetry } from "volcano-sdk";
+
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+
+const serviceName = process.env.OTEL_SERVICE_NAME || "volcano-sandbox";
+const otlpEndpoint =
+  process.env.OTEL_EXPORTER_OTLP_ENDPOINT || "http://localhost:4318";
+const gatewayEndpoint = process.env.GATEWAY_ENDPOINT || "http://localhost:8000"
+
+
+const sdk = new NodeSDK({
+  serviceName: serviceName,
+  traceExporter: new OTLPTraceExporter({
+    url: `${otlpEndpoint}/v1/traces`,
+  }),
+});
+
+sdk.start();
+
+// Graceful shutdown
+process.on("SIGTERM", async () => {
+  await sdk.shutdown();
+});
+
+const telemetry = createVolcanoTelemetry({
+  serviceName: serviceName,
+});
+
+const llm = llmOpenAI({
+  apiKey: process.env.OPENAI_API_KEY!,
+  model: "gpt-4o-mini",
+  baseURL: `${gatewayEndpoint}/openai`
+});
+
+try {
+  const results = await agent({
+    llm,
+    telemetry, // Enable observability feature.
+  })
+    .then({ prompt: "Volcano SDKとはなんですか？" })
+    .run();
+  const firstResult = results[0];
+  if (!firstResult || !firstResult.llmOutput) {
+    throw new Error("The agent returned no results.");
+  }
+  console.log(firstResult.llmOutput);
+} catch (error) {
+  throw new Error("The agent returned no results.");
+}
